b2b659484dcd49ee2adad6787540d2e1668035f4
batnyu <vrignaudbaptiste@gmail.com>
2020-02-18 15:45:52 +0100

    feat: elastic search search and suggest

--- app/repository/ElasticRepository.java
+++ app/repository/ElasticRepository.java
@@ -1,15 +1,19 @@
 package repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import env.ElasticConfiguration;
 import env.MarvelHeroesConfiguration;
 import models.PaginatedResults;
 import models.SearchedHero;
+import play.libs.Json;
 import play.libs.ws.WSClient;
 import utils.SearchedHeroSamples;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
@@ -28,22 +32,63 @@ public class ElasticRepository {
 
 
     public CompletionStage<PaginatedResults<SearchedHero>> searchHeroes(String input, int size, int page) {
-        return CompletableFuture.completedFuture(new PaginatedResults<>(3, 1, 1, Arrays.asList(SearchedHeroSamples.IronMan(), SearchedHeroSamples.MsMarvel(), SearchedHeroSamples.SpiderMan())));
+//        return CompletableFuture.completedFuture(new PaginatedResults<>(3, 1, 1, Arrays.asList(SearchedHeroSamples.IronMan(), SearchedHeroSamples.MsMarvel(), SearchedHeroSamples.SpiderMan())));
         // TODO
-        // return wsClient.url(elasticConfiguration.uri + "...")
-        //         .post(Json.parse("{ ... }"))
-        //         .thenApply(response -> {
-        //             return ...
-        //         });
+        int from = ((page - 1) * size);
+        return wsClient.url(elasticConfiguration.uri + "/heroes/_search")
+                .post(Json.parse("{" +
+                        "  \"from\": " + from + "," +
+                        "  \"size\": " + size + "," +
+                        "  \"query\": {\n" +
+                        "    \"multi_match\" : {\n" +
+                        "      \"query\" : \"" + input + "\",\n" +
+                        "      \"fields\" : [ \"name^4\", \"aliases^3\", \"secretIdentities^3\", \"description^2\", \"partners^1\"] \n" +
+                        "    }\n" +
+                        "  }" +
+                        "}"))
+                .thenApply(response -> {
+                    List<SearchedHero> searchedHeroes = new ArrayList<>();
+                    int total = response.asJson().get("hits").get("total").get("value").asInt();
+                    Iterator<JsonNode> it = response.asJson().get("hits").get("hits").iterator();
+                    while (it.hasNext()) {
+                        searchedHeroes.add(SearchedHero.fromJson(it.next().get("_source")));
+                    }
+                    int totalPage = (int) Math.ceil(total / size);
+                    if (totalPage == 0) {
+                        totalPage = 1;
+                    }
+                    System.out.println("total " + total);
+                    System.out.println("size " + size);
+                    System.out.println("totalPage " + totalPage);
+                    System.out.println("page " + page);
+                    return new PaginatedResults<>(total, page, totalPage, searchedHeroes);
+                });
     }
 
     public CompletionStage<List<SearchedHero>> suggest(String input) {
-        return CompletableFuture.completedFuture(Arrays.asList(SearchedHeroSamples.IronMan(), SearchedHeroSamples.MsMarvel(), SearchedHeroSamples.SpiderMan()));
         // TODO
-        // return wsClient.url(elasticConfiguration.uri + "...")
-        //         .post(Json.parse("{ ... }"))
-        //         .thenApply(response -> {
-        //             return ...
-        //         });
+        return wsClient.url(elasticConfiguration.uri + "/heroes/_search")
+                .post(Json.parse("{" +
+                        "    \"suggest\": {\n" +
+                        "        \"heroes-suggest\" : {\n" +
+                        "            \"prefix\" : \"" + input + "\", \n" +
+                        "            \"completion\" : { \n" +
+                        "                \"field\" : \"name_suggest\" \n" +
+                        "            }\n" +
+                        "        }\n" +
+                        "    }" +
+                        "}"))
+                .thenApply(response -> {
+                    System.out.println(response.asJson());
+                    List<SearchedHero> searchedHeroes = new ArrayList<>();
+                    Iterator<JsonNode> it = response.asJson().get("suggest").get("heroes-suggest").get(0).get("options").iterator();
+                    while (it.hasNext()) {
+                        JsonNode jsonNode = it.next();
+                        SearchedHero searchedHero = SearchedHero.fromJson(jsonNode.get("_source"));
+                        searchedHeroes.add(searchedHero);
+                    }
+                    return searchedHeroes;
+                });
+
     }
 }
--- scripts/import-elasticsearch.js
+++ scripts/import-elasticsearch.js
@@ -1,9 +1,95 @@
 const csv = require('csv-parser');
 const fs = require('fs');
+const {bufferCount, concatMap, delay} = require('rxjs/operators');
+const {from, of} = require('rxjs');
 
-const { Client } = require('@elastic/elasticsearch')
-const esClient = new Client({ node: 'http://localhost:9200' })
-const heroesIndexName = 'heroes'
+const {Client} = require('@elastic/elasticsearch')
+const indexName = 'heroes';
 
-// TODO
-console.log("TODO ;-)");
+async function run() {
+    // Create Elasticsearch client
+    const client = new Client({node: 'http://localhost:9200'});
+
+    // Création de l'indice
+    client.indices.create({index: indexName}, (err, resp) => {
+        if (err) console.trace(err.message);
+    });
+
+    let heroes = [];
+    let counter = 0;
+    // Read CSV file
+    fs.createReadStream('all-heroes.csv')
+        .pipe(csv({
+            separator: ','
+        }))
+        .on('data', (data) => {
+            heroes.push({
+                id: data["id"],
+                name: data["name"],
+                description: data["description"],
+                secretIdentities: data["secretIdentities"],
+                aliases: data["aliases"],
+                partners: data["partners"],
+                imageUrl: data["imageUrl"],
+                gender: data["gender"],
+                universe: data["universe"]
+            });
+        })
+        .on('end', () => {
+            from(heroes).pipe(
+                bufferCount(200),
+                concatMap(data => of(data).pipe(delay(1000)))
+            ).subscribe(bufferedHeroes => {
+                counter += bufferedHeroes.length;
+                console.log(counter);
+                client.bulk(createBulkInsertQuery(bufferedHeroes), (err, resp) => {
+                    if (err) console.trace(err.message);
+                    else console.log(`Inserted ${resp.body.items.length} heroes`);
+                    // client.close();
+                });
+            });
+        });
+}
+
+// Fonction utilitaire permettant de formatter les données pour l'insertion "bulk" dans elastic
+function createBulkInsertQuery(heroes) {
+    const body = heroes.reduce((acc, hero) => {
+        let {secretIdentities, aliases, partners, ...rest} = hero;
+
+        secretIdentities = split(secretIdentities);
+        aliases = split(aliases);
+        partners = split(partners);
+
+        let name_suggest = [];
+        name_suggest.push(formatInputWeight(rest.name, 10));
+        secretIdentities.forEach(s => name_suggest.push(formatInputWeight(s, 5)));
+        aliases.forEach(a => name_suggest.push(formatInputWeight(a, 5)));
+
+        const heroFormatted = {
+            ...rest,
+            secretIdentities,
+            aliases,
+            partners,
+            name_suggest
+        };
+
+        acc.push({index: {_index: indexName, _type: '_doc', _id: hero.id}});
+        acc.push(heroFormatted);
+        return acc
+    }, []);
+
+    return {body};
+}
+
+function split(str) {
+    return str ? str.split(',') : [];
+}
+
+function formatInputWeight(input, weight) {
+    return {
+        input,
+        weight
+    }
+}
+
+run().catch(console.error);
