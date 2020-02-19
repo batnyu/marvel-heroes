c0f526911b5b9619cda0f7fa02ffca222ec30ae4
batnyu <vrignaudbaptiste@gmail.com>
2020-02-19 13:59:12 +0100

    fix: import-mongo with rxjs

--- scripts/import-mongo.js
+++ scripts/import-mongo.js
@@ -1,8 +1,8 @@
 var mongodb = require("mongodb");
 var csv = require("csv-parser");
 var fs = require("fs");
-const {bufferCount, concatMap, delay} = require('rxjs/operators');
-const {from, of} = require('rxjs');
+const {bufferCount, concatMap, delay, flatMap, concat, tap, startWith} = require('rxjs/operators');
+const {from, of, Observable} = require('rxjs');
 
 var MongoClient = mongodb.MongoClient;
 var mongoUrl = "mongodb://localhost:27017";
@@ -14,56 +14,85 @@ const sourcePath = "all-heroes.csv";
 const bufferSize = 200;
 const delayBetweenImport = 500; // ms
 
-let counter = 0;
-
 function run() {
 
     MongoClient.connect(
         mongoUrl,
-        { useNewUrlParser: true, useUnifiedTopology: true },
-        async (err, client) =>  {
-
+        {useNewUrlParser: true, useUnifiedTopology: true},
+        (err, client) => {
             if (err) throw err;
             let db = client.db(dbName).collection(collectionName);
-
-            db.deleteMany({}, function(err, numberOfRemovedDocs) {
-                if(err) throw err;
-                console.log("Dropped " + numberOfRemovedDocs + " documents")
-            });
-
-            insertFromCsv(sourcePath, db);
+            deleteExistingAndInsertFromCsv(sourcePath, db, client);
         }
     );
 }
 
-function insertFromCsv(sourcePath, db){
-    readCsv(sourcePath, (heroes) => insertData(heroes, db));
+async function deleteExistingAndInsertFromCsv(sourcePath, db, client) {
+
+    const source = deleteMany(db).pipe(
+        flatMap(() => readCsv(sourcePath)),
+        flatMap(heroes => insertData(heroes, db)),
+        concat(of([]).pipe(
+            tap(() => {
+                console.log("FINISHED");
+                client.close();
+            })
+        ))
+    );
+
+    source.subscribe();
 }
 
-function readCsv(sourcePath, callback){
-    let heroes = [];
-    fs.createReadStream(sourcePath)
-        .pipe(csv({
-            separator: ","
-        }))
-        .on("data", (data) => heroes.push(parseHero(data)))
-        .on("end", () => {
-            callback(heroes);
-        });
-}
-
-function insertData(csvdata, db){
-    from(csvdata).pipe(
-        bufferCount(bufferSize),
-        concatMap(data => of(data).pipe(delay(delayBetweenImport)))
-    ).subscribe(bufferedData => {
-        counter += bufferedData.length;
-        console.log(counter);
+function insertMany(db, bufferedData) {
+    return new Observable(observer => {
         db.insertMany(bufferedData, (err, res) => {
-            if (err) throw err;
+            if (err) observer.error(err);
             console.log(`Inserted: ${res.insertedCount} rows`);
+            observer.next(bufferedData);
+            observer.complete();
         });
+    })
+}
+
+function deleteMany(db) {
+    return new Observable(observer => {
+        db.deleteMany({}, function (err, numberOfRemovedDocs) {
+            if (err) throw err;
+            console.log("Dropped " + numberOfRemovedDocs.result.n + " documents");
+            observer.next();
+            observer.complete();
+        });
+    })
+}
+
+function readCsv(sourcePath) {
+    return new Observable(observer => {
+        let heroes = [];
+        fs.createReadStream(sourcePath)
+            .pipe(csv({
+                separator: ","
+            }))
+            .on("data", (data) => heroes.push(parseHero(data)))
+            .on("end", () => {
+                observer.next(heroes);
+                observer.complete();
+            });
     });
+}
+
+function insertData(csvdata, db) {
+    let counter = 0;
+    return from(csvdata).pipe(
+        bufferCount(bufferSize),
+        concatMap(data => of(data).pipe(
+            flatMap(bufferedData => insertMany(db, bufferedData)),
+            tap(bufferedData => {
+                counter += bufferedData.length;
+                console.log("Actual counter: " + counter);
+            }),
+            delay(delayBetweenImport))
+        )
+    );
 }
 
 function toArray(string) {
@@ -78,57 +107,57 @@ function toFloat(string) {
     return string && (typeof string === "string") ? parseFloat(string) : null;
 }
 
-function parseHero(data){
+function parseHero(data) {
     return {
-        id : data["id"],
-        name : data["name"],
-        imageUrl : data["imageUrl"],
-        backgroundImageUrl : data["backgroundImageUrl"],
-        externalLink : data["externalLink"],
-        description : data["description"],
-        identity : parseIdentity(data),
-        appearance : parseAppearance(data),
-        teams : toArray(data["teams"]),
-        powers : toArray(data["powers"]),
-        partners : toArray(data["partners"]),
-        skills : parseSkills(data),
-        creators : toArray(data["creators"])
+        id: data["id"],
+        name: data["name"],
+        imageUrl: data["imageUrl"],
+        backgroundImageUrl: data["backgroundImageUrl"],
+        externalLink: data["externalLink"],
+        description: data["description"],
+        identity: parseIdentity(data),
+        appearance: parseAppearance(data),
+        teams: toArray(data["teams"]),
+        powers: toArray(data["powers"]),
+        partners: toArray(data["partners"]),
+        skills: parseSkills(data),
+        creators: toArray(data["creators"])
     }
 }
 
-function parseIdentity(data){
+function parseIdentity(data) {
     return {
-        secretIdentities : toArray(data["secretIdentities"]),
-        birthPlace : data["birthPlace"],
-        occupation : data["occupation"],
-        aliases : toArray(data["aliases"]),
-        alignment : data["alignment"],
-        firstAppearance : data["firstAppearance"],
-        yearAppearance : toNumber(data["yearAppearance"]),
-        universe : data["universe"]
+        secretIdentities: toArray(data["secretIdentities"]),
+        birthPlace: data["birthPlace"],
+        occupation: data["occupation"],
+        aliases: toArray(data["aliases"]),
+        alignment: data["alignment"],
+        firstAppearance: data["firstAppearance"],
+        yearAppearance: toNumber(data["yearAppearance"]),
+        universe: data["universe"]
     }
 }
 
-function parseAppearance(data){
+function parseAppearance(data) {
     return {
-        gender : data["gender"],
-        type : data["type"],
-        race : data["race"],
-        height : toFloat(data["height"]),
-        weight : toFloat(data["weight"]),
-        eyeColor : data["eyeColor"],
-        hairColor : data["hairColor"]
+        gender: data["gender"],
+        type: data["type"],
+        race: data["race"],
+        height: toFloat(data["height"]),
+        weight: toFloat(data["weight"]),
+        eyeColor: data["eyeColor"],
+        hairColor: data["hairColor"]
     }
 }
 
-function parseSkills(data){
+function parseSkills(data) {
     return {
-        intelligence : toNumber(data["intelligence"]),
-        strength : toNumber(data["strength"]),
-        speed : toNumber(data["speed"]),
-        durability : toNumber(data["durability"]),
-        combat : toNumber(data["combat"]),
-        power : toNumber(data["power"])
+        intelligence: toNumber(data["intelligence"]),
+        strength: toNumber(data["strength"]),
+        speed: toNumber(data["speed"]),
+        durability: toNumber(data["durability"]),
+        combat: toNumber(data["combat"]),
+        power: toNumber(data["power"])
     }
 }
 
